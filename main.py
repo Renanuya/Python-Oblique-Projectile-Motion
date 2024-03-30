b79a7dcd9382661a85757376e5f8ad01cc8a8774
renanuya <renanuya@gmail.com>
2024-03-30 22:50:16 +0300

    updated code

--- main.py
+++ main.py
@@ -1,54 +1,65 @@
-# Physics Engine version 1.0
-# This is a simple physics engine that calculates the angle and velocity of a rocket to hit a ship
-# Oblique Projectile Motion
-
+# Physics Engine version 1.1
 import math
+import random
 
-rocketLaunchAngle = 0
-rocketLaunchVelocity = 0
-rocketXAxisLocation = 10
+import numpy as np
 
-rocketLaunchFlyTime = 0
-rocketLaunchTryTime = 0
+bullet_max_velocity = 1800
+bullet_min_velocity = 330
+bullet_velocity = (bullet_max_velocity + bullet_min_velocity) / 2
+# Metres per second
 
-gravity = 9.8
+bullet_angle = 30
+# Degrees
 
-doesRocketHitTheShip = True
+bullet_fly_time = 0
+# Seconds
 
-print("Welcome to the Physics Engine version 1.0")
-shipXAxisLocation = int(input("Please enter the location of the ship in x-axis between 0 and 1000 "))
+bullet_fly_distance = 0
+# Metres
 
-while doesRocketHitTheShip:
-    if (shipXAxisLocation - 50) < rocketXAxisLocation < (shipXAxisLocation + 50):
-        print("You hit the ship! Congratulations!")
-        print("Physics Engine Details: ")
-        print("Rocket Angle: " + str(rocketLaunchAngle))
-        print("Rocket Velocity: " + str(rocketLaunchVelocity))
-        print("Rocket Fly Time: " + str(rocketLaunchFlyTime))
-        print("Rocket Max height: " + str(round(rocketLaunchYAxisVelocity ** 2 / (2 * gravity))))
-        print("Rocket X-Axis Location: " + str(rocketXAxisLocation))
-        print("Try Time: " + str(rocketLaunchTryTime))
+target_distance = 20000 + (200 * random.randint(-10, 10))
+# Metres in x-axis
 
-        doesRocketHitTheShip = False
+target_size = 1000 + (100 * random.randint(-2, 2))
+# Metres in x-axis
+
+firing_point_ground_clearance = 21
+# Metres in y-axis
+
+shot_attempts = 0
+# Number of shots fired
+
+hit_target = False
+
+print("Welcome to the Physics Engine version 1.1")
+
+while not hit_target:
+    if target_distance <= bullet_fly_distance <= target_distance + target_size:
+        hit_target = True
+        print("Target hit!")
+        print("Number of shots fired: ", shot_attempts)
+        print("Bullet velocity: ", bullet_velocity)
+        print("Bullet angle: ", bullet_angle)
+        print("Bullet fly time: ", bullet_fly_time)
+        print("Bullet fly distance: ", bullet_fly_distance)
+        print("Target distance: ", target_distance)
+        print("Target size: ", target_size)
+        print("Firing point ground clearance: ", firing_point_ground_clearance)
+        break
     else:
-        rocketLaunchTryTime += 1
-        rocketLaunchAngle = round(int(input("Please enter the angle of the rocket between 1 and 89 ")))
-        rocketLaunchVelocity = round(int(input("Please enter the velocity of the rocket NO LIMIT ")))
+        bullet_x_axis_velocity = round((math.sin(math.radians(bullet_angle))) * bullet_velocity)
+        bullet_y_axis_velocity = round((math.cos(math.radians(bullet_angle))) * bullet_velocity)
 
-        rocketLaunchYAxisVelocity = round((math.sin(math.radians(rocketLaunchAngle))) * rocketLaunchVelocity)
-        rocketLaunchXAxisVelocity = round((math.cos(math.radians(rocketLaunchAngle))) * rocketLaunchVelocity)
+        bullet_fly_time = round((bullet_y_axis_velocity / 9.8) / 2)
+        bullet_fly_distance = round(bullet_x_axis_velocity * bullet_fly_time)
 
-        rocketLaunchFlyTime = round((rocketLaunchYAxisVelocity / gravity) * 2)
-        rocketXAxisLocation = round(rocketLaunchXAxisVelocity * rocketLaunchFlyTime)
-
-        if rocketXAxisLocation > (shipXAxisLocation + 50):
-            print("You missed the ship! Try again!")
-
-            print("The rocket fell +" + str(rocketXAxisLocation - (shipXAxisLocation + 50)) + "m away from the ship")
+        if target_distance >= bullet_fly_distance:
+            print("Mermi öne düştü")
+            bullet_velocity = bullet_velocity + 20
+        elif bullet_fly_distance >= target_distance + target_size:
+            print("The bullet fell behind the target!")
+            bullet_velocity = bullet_velocity - 20
         else:
-            if (shipXAxisLocation - 50) < rocketXAxisLocation < (shipXAxisLocation + 50):
-                pass
-            else:
-                print("You missed the ship! Try again!")
-
-                print("The rocket fell -" + str((shipXAxisLocation - 50) - rocketXAxisLocation) + "m away from the ship")
+            print("Something went wrong!")
+        shot_attempts += 1
